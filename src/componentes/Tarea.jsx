397ae69d9b854e41186f784524d15cf27fc4deb7
Micaela Korol <84535869+micaelakorol@users.noreply.github.com>
2022-04-28 16:50:17 -0300

    Update Tarea.jsx

--- src/componentes/Tarea.jsx
+++ src/componentes/Tarea.jsx
@@ -2,52 +2,47 @@
   import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' 
   import { faCheckSquare, faEdit,faSquare,faTimes } from '@fortawesome/free-solid-svg-icons'
 
-  //accedo a las tareas que agrego el usuario. (traigo la funcion toggle) para el check:
-
   const Tarea = ({item, toggleCompletada , editarTareas, borrarTarea}) => {
 
-  const [editarTarea , setEditarTarea] = useState(false) //parte en false = no se muestra
-  const [nuevaTarea, setNuevaTarea] = useState(item.texto) /// ---->
-  /* ---> aca , lo que hago es un condicional, donde: si el estado es falso, se muestra el item.texto
-  caso contrario , si es true, muestra la opcion de editar y actualizar (formulario del condicional)*/
-
+  const [editarTarea , setEditarTarea] = useState(false)
+  const [nuevaTarea, setNuevaTarea] = useState(item.texto)
+  
+//---------------------------------------------------
   const handleSubmit = (e) => {
     e.preventDefault()
-    editarTareas(item.id , nuevaTarea) //si los id coinciden, reescribe la tarea por la nueva q ingresamos en el value 
-  setEditarTarea(false) //volvemos a cambiar el estado
+    editarTareas(item.id , nuevaTarea) 
+  setEditarTarea(false)
   }
     return (
   <>
     <li className='lista-tareas__tarea'>
-    <FontAwesomeIcon icon={item.completada ? faCheckSquare : faSquare} /* 
-   si la tarea esta completada muestra el icono faCheck.. caso contrario muestra faSquare */
+    <FontAwesomeIcon icon={item.completada ? faCheckSquare : faSquare}
      className='lista-tareas__icono lista-tareas__icono-check'
-     onClick={() => toggleCompletada(item.id)}/> {/*le pasamos el id de la tarea que ingreso el user,
-     para que compare en la funcion toggle a ver en que estado está la misma */}
+     onClick={() => toggleCompletada(item.id)}/> 
+
     <div className='lista-tareas__texto'>{editarTarea ? 
    <form action="" className='formulario-editar-tarea' onSubmit={handleSubmit}>
   <input type="text" className='formulario-editar-tarea__input'
 
   /*leer y acceder al valor de la nueva tarea*/
-  onChange={(e) => setNuevaTarea(e.target.value)} /*accede a la nueva tarea; value: capta la nueva tarea*/ 
+  onChange={(e) => setNuevaTarea(e.target.value)} 
   value={nuevaTarea}/> 
   <button type='submit' className='formulario-editar-tarea__btn'
   >Actualizar</button>
   </form>  
-  : item.texto /*caso contrario se muestra la tarea ingresada, es decir, la que no se modifico*/
+  : item.texto 
   }
   </div>
+
   <div className='lista-tareas__botones'>
   <FontAwesomeIcon icon={faEdit} 
   className='lista-tareas__icono lista-tareas__icono-accion'
-  /*hacemos que cuando se presione el boton editar, se cambie el estado en el contrario al que se
-  encuentra: true a false y viceversa*/
+  
   onClick={() => setEditarTarea(!editarTarea)}/>
   <FontAwesomeIcon icon={faTimes} 
   className='lista-tareas__icono lista-tareas__icono-accion'
   onClick={()=> borrarTarea(item.id)}
   />
-
   </div></li> 
       
   </> 
